4b6e5d484a7e7df776ebeb52b729078e4977f64d
Ahron Pollak <ahronrocks51@gmail.com>
2024-11-26 13:00:27 -0500

    Implement entroy

--- ch2_prelims/cmd/prelims/main.go
+++ ch2_prelims/cmd/prelims/main.go
@@ -31,9 +31,54 @@ func main() {
 		}
 	}
 
-	info, err := information.SelfInformation(probability, base)
+	/*
+		info, err := information.SelfInformation(probability, base)
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Printf("The self-information associated with the probability %.2f is %.3f\n", probability, info)
+	*/
+	/*
+		probs := []float64{1.0 / 16.0, 2.0 / 16.0}
+		entropy, err := information.Entropy(probs, base)
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Printf("Entropy: %.3f\n", entropy)
+	*/
+	/*
+		sampleSpace := []int64{1, 2, 3, 2, 3, 4, 5, 4, 5, 6, 7, 8, 9, 8, 9, 10}
+		entropy, err := information.Entropy(sampleSpace, base)
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Printf("%.3f\n", entropy)
+
+		residualSpace := []int64{1, 1, 1, -1, 1, 1, 1, -1, 1, 1, 1, 1, 1, -1, 1, 1}
+		entropyCorrelation, err := information.Entropy(residualSpace, base)
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Printf("%.2f\n", entropyCorrelation)
+	*/
+
+	threeA := []int64{1, 2, 3, 4}
+	threeAEntropy, err := information.Entropy(threeA, base)
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Printf("The self-information associated with the probability %.2f is %.3f\n", probability, info)
+	fmt.Printf("%.2f\n", threeAEntropy)
+	threeB := []float64{1.0 / 2, 1.0 / 4, 1.0 / 8, 1.0 / 8}
+	threeBEntropy, err := information.EntropyProbabilities(threeB, base)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("%.2f\n", threeBEntropy)
+
+	threeC := []float64{0.505, 1.0 / 4, 1.0 / 8, 0.12}
+	threeCEntropy, err := information.EntropyProbabilities(threeC, base)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("%.2f\n", threeCEntropy)
 }
--- ch2_prelims/information/information.go
+++ ch2_prelims/information/information.go
@@ -5,7 +5,7 @@ import (
 	"math"
 )
 
-// information calculates the self-information from the probability
+// SelfInformation calculates the self-information from the probability
 // of some event A.
 func SelfInformation(probability float64, base float64) (float64, error) {
 	if base <= 0 {
@@ -20,4 +20,43 @@ func SelfInformation(probability float64, base float64) (float64, error) {
 	return -math.Log(probability) / math.Log(base), nil
 }
 
-func Entropy() {}
+// Entropy calculates the entropy for a given sample space
+func Entropy(sampleSpace []int64, base float64) (float64, error) {
+	total := len(sampleSpace)
+	if total == 0 {
+		return 0.0, fmt.Errorf("empty sample space")
+	}
+
+	// Count the occurrence of each value
+	frequencies := make(map[int64]int64)
+	for _, v := range sampleSpace {
+		frequencies[v]++
+	}
+
+	// Accumulate information values for each value in the sample space,
+	// and then return the avg information, the entropy.
+	var entropy float64
+	for k, v := range frequencies {
+		prob := float64(v) / float64(total)
+		fmt.Printf("prob for k %d is: %f\n", k, prob)
+
+		selfInfo, err := SelfInformation(prob, base)
+		if err != nil {
+			return 0, nil
+		}
+		entropy += prob * selfInfo
+	}
+	return entropy, nil
+}
+
+func EntropyProbabilities(probabilities []float64, base float64) (float64, error) {
+	var entropy float64
+	for _, prob := range probabilities {
+		selfInfo, err := SelfInformation(prob, base)
+		if err != nil {
+			return 0, err
+		}
+		entropy += prob * selfInfo
+	}
+	return entropy, nil
+}
