961543b2256f27f3b7c7567e3213fc5966a5e402
Ahron Pollak <ahronrocks51@gmail.com>
2024-12-02 14:44:25 -0500

    Implement Primitive Huffman Tree

--- huffman/cmd/main.go
+++ huffman/cmd/main.go
@@ -7,8 +7,45 @@ import (
 )
 
 func main() {
-	probs := []float64{0.4, 0.2, 0.2, 0.1, 0.1}
-	bits := []int{1, 2, 3, 4, 4}
-	avgLen := hufftree.AverageLength(probs, bits)
-	fmt.Println(avgLen, "bits/symbol")
+	/*
+		probs := []float64{0.4, 0.2, 0.2, 0.1, 0.1}
+		bits := []int{1, 2, 3, 4, 4}
+		avgLen := hufftree.AverageLength(probs, bits)
+		redun := hufftree.Redundancy(2.122, avgLen)
+		fmt.Println("Average Length:", avgLen, "bits/symbol")
+		fmt.Printf("Redundancy: %.3f bits/symbol\n", redun)
+	*/
+
+	// Using example 2.3.1
+	symbols := []rune{'a', 'b', 'c', 'd', 'e'}
+	probs := []float64{0.2, 0.4, 0.2, 0.1, 0.1}
+
+	var nodes []*hufftree.Node
+	for i, symbol := range symbols {
+		nodes = append(nodes, hufftree.NewLeafNode(symbol, probs[i]))
+	}
+
+	huffManTree := hufftree.BuildHuffmanTree(nodes)
+
+	codes := make(map[rune]string)
+
+	huffManTree.AssignCodes(huffManTree.Root, "", codes)
+
+	hufftree.PrintHuffmanTree(huffManTree.Root)
+
+	for l, c := range codes {
+		fmt.Printf("%c: %s\n", l, c)
+	}
+
+	// Extract each code length and append it to a slice
+	var codeLengths []int
+	for _, s := range symbols {
+		if c, ok := codes[s]; ok {
+			codeLengths = append(codeLengths, len(c))
+		}
+	}
+	avgLen := hufftree.AverageLength(probs, codeLengths)
+	redundancy := hufftree.Redundancy(2.122, avgLen)
+	fmt.Printf("Average length of Huffman Code: %.2f\n", avgLen)
+	fmt.Printf("Redundancy of Huffman Code: %.2f\n", redundancy)
 }
--- huffman/tree/huff_tree.go
+++ huffman/tree/huff_tree.go
@@ -1,5 +1,11 @@
 package hufftree
 
+import (
+	"fmt"
+	"log"
+	"sort"
+)
+
 // Node represents a node in a Huffman Tree. It assumes the probability is known.
 type Node struct {
 	letter      rune
@@ -9,16 +15,16 @@ type Node struct {
 
 // HuffmanTree is simply a wrapper around the root of a HuffmanTree.
 type HuffmanTree struct {
-	root *Node
+	Root *Node
 }
 
 func NewHuffmanTree(root *Node) *HuffmanTree {
 	return &HuffmanTree{
-		root: root,
+		Root: root,
 	}
 }
 
-func NewNode(letter rune, probability float64) *Node {
+func NewLeafNode(letter rune, probability float64) *Node {
 	return &Node{
 		letter:      letter,
 		probability: probability,
@@ -27,10 +33,81 @@ func NewNode(letter rune, probability float64) *Node {
 	}
 }
 
-// TODO: Implement a method for building a Huffman Tree.
-// TODO: Implement sorting.
-// TODO: Implement frequency count of letters from an alphabet.
-// TODO: Implement average length of code.
+// BuildHuffmanTree will build a Huffman tree.
+func BuildHuffmanTree(nodes []*Node) *HuffmanTree {
+	// Step 1: Sort the nodes by probability
+	sort.Slice(nodes, func(i, j int) bool {
+		return nodes[i].probability < nodes[j].probability
+	})
+
+	// Step 2: Combine nodes until one node (the root) is left
+	for len(nodes) > 1 {
+		//  Step 2a: Take the two nodes with the lowest probability
+		l := nodes[0]
+		r := nodes[1]
+
+		//  Step 2b: Create a new internal node that combines these
+		// Internal node
+		newNode := &Node{
+			letter:      0, // only leaves have a rune (letter)
+			probability: l.probability + r.probability,
+			left:        l,
+			right:       r,
+		}
+		// fmt.Println("Combining nodes into internal node:", newNode)
+
+		// fmt.Printf("%#v\n", nodes)
+		//  Step 2c: Remove these nodes and add the new one
+		nodes = nodes[2:]
+
+		//  Step 2d: Add this new node to our "sorter"
+		nodes = append(nodes, newNode)
+		// fmt.Println("Combining nodes")
+		sort.Slice(nodes, func(i, j int) bool {
+			return nodes[i].probability < nodes[j].probability
+		})
+
+		for _, n := range nodes {
+			fmt.Printf("%c %.3f\n", n.letter, n.probability)
+		}
+	}
+
+	// Step 3: Return the root node
+	return &HuffmanTree{
+		Root: nodes[0],
+	}
+}
+
+// PrintHuffmanTree performs an inorder traversal of the Huffman Tree.
+func PrintHuffmanTree(node *Node) {
+	if node == nil {
+		return
+	}
+	PrintHuffmanTree(node.left)
+	fmt.Printf("%#v\n", node)
+	PrintHuffmanTree(node.right)
+}
+
+// Traverse the Huffman tree to assign binary codewords to each symbol.
+func (tree *HuffmanTree) AssignCodes(node *Node, code string, codes map[rune]string) {
+	if node == nil {
+		return
+	}
+
+	// Assign the code to a leaf node.
+	if node.left == nil && node.right == nil {
+		if node.letter != 0 {
+			codes[node.letter] = code
+		}
+		return
+	}
+
+	// Traverse the left and right subtrees
+	tree.AssignCodes(node.left, code+"0", codes)
+	tree.AssignCodes(node.right, code+"1", codes)
+}
+
+// AverageLength computes the average length of a binary code.
 func AverageLength(probabilities []float64, bits []int) float64 {
 	var length float64
 	for i := 0; i < len(bits); i++ {
@@ -39,6 +116,10 @@ func AverageLength(probabilities []float64, bits []int) float64 {
 	return length
 }
 
+// Redundancy is a measure of efficiency a given code.
 func Redundancy(entropy float64, avgLen float64) float64 {
-	return entropy / avgLen
+	if avgLen < entropy {
+		log.Fatalf("average length can not be less than entropy")
+	}
+	return avgLen - entropy
 }
