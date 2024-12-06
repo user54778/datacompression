00d608c0971f458bfe6d7910070fd8b442316cc2
Ahron Pollak <ahronrocks51@gmail.com>
2024-12-06 15:54:28 -0500

    Implement Tunstall Code Creation and Codebook

--- tunstall/cmd/main.go
+++ tunstall/cmd/main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"container/heap"
 	"fmt"
+	"log"
+	"strings"
 
 	"tunstall.adpollak.net/tunstalltree"
 )
@@ -11,10 +13,27 @@ func main() {
 	a := tunstalltree.Node{Sequence: "a", Probability: 0.6}
 	b := tunstalltree.Node{Sequence: "b", Probability: 0.3}
 	c := tunstalltree.Node{Sequence: "c", Probability: 0.1}
+	nodes := []*tunstalltree.Node{&a, &b, &c}
 	h := &tunstalltree.MaxHeap{a, b, c}
 	heap.Init(h)
-	fmt.Printf("maximium :%#v\n", (*h)[0])
-	for h.Len() > 0 {
-		fmt.Printf("%#v\n", heap.Pop(h))
+	/*
+		fmt.Printf("maximium :%#v\n", (*h)[0])
+		for h.Len() > 0 {
+			fmt.Printf("%#v\n", heap.Pop(h))
+		}
+	*/
+
+	codebook := make(map[string]string)
+	var highest []*tunstalltree.Node
+	nodes, highest = tunstalltree.BuildTunstallCode(h, 3, nodes)
+	tunstalltree.AssignTunstallCodes(nodes, codebook, 3, highest)
+
+	input := "aaabaabaabaabaaa"
+	fmt.Println(input)
+	fmt.Println(codebook)
+	encoded, err := tunstalltree.TunstallEncode(strings.ToLower(input), codebook)
+	if err != nil {
+		log.Fatal(err)
 	}
+	fmt.Println(encoded)
 }
--- tunstall/tunstalltree/tunstalltree.go
+++ tunstall/tunstalltree/tunstalltree.go
@@ -1,5 +1,12 @@
 package tunstalltree
 
+import (
+	"container/heap"
+	"fmt"
+	"math"
+	"strings"
+)
+
 // IMPLEMENT ME: Implement an n-bit Tunstall code
 // for a source that generates iid letters from an alphabet of size N.
 // This should be done with a Heap.
@@ -8,6 +15,21 @@ package tunstalltree
 type Node struct {
 	Sequence    string
 	Probability float64
+	Left        *Node
+	Right       *Node
+}
+
+type TunstallTree struct {
+	Root *Node
+}
+
+func NewLeafNode(sequence string, probability float64) *Node {
+	return &Node{
+		Sequence:    sequence,
+		Probability: probability,
+		Left:        nil,
+		Right:       nil,
+	}
 }
 
 // Step 2: We can use a Node slice to implement on the Heap interface.
@@ -32,20 +54,140 @@ func (h *MaxHeap) Pop() any {
 	return x
 }
 
+func BuildTunstallCode(maxHeap *MaxHeap, n int, nodes []*Node) ([]*Node, []*Node) {
+	var highests []*Node
+	for maxHeap.Len() < (1 << n) {
+		top := heap.Pop(maxHeap).(Node)
+		highests = append(highests, &top)
+
+		var leafNode *Node
+		for _, n := range nodes {
+			newProb := top.Probability * n.Probability
+			newPrefix := top.Sequence + n.Sequence
+			newProb = roundProbability(newProb)
+
+			leafNode = &Node{
+				Sequence:    newPrefix,
+				Probability: newProb,
+				Left:        &top,
+				Right:       n,
+			}
+
+			nodes = append(nodes, leafNode)
+			heap.Push(maxHeap, Node{newPrefix, newProb, nil, nil})
+		}
+	}
+
+	return nodes, highests
+}
+
+func AssignTunstallCodes(nodes []*Node, codebook map[string]string, n int, highest []*Node) {
+	code := 0
+	var flag bool
+	for _, v := range nodes {
+		fmt.Println(v)
+		// Skip the highest probability node (this is the one that was split)
+		if v.Left == nil && v.Right == nil && v.Probability == highest[0].Probability {
+			continue
+		}
+		if len(codebook) == (1 << n) {
+			break
+		}
+		if highest[1].Probability == v.Probability && !flag {
+			flag = true
+			continue
+		}
+
+		binaryCode := fmt.Sprintf("%0*b", n, code)
+		codebook[v.Sequence] = binaryCode
+
+		code++
+	}
+}
+
+func roundProbability(f float64) float64 {
+	return math.Round(f*100) / 100
+}
+
+func removeNode(nodes []*Node, index int) []*Node {
+	toRemove := len(nodes) - index
+	fmt.Println("toRemove:", toRemove)
+	ret := make([]*Node, 0)
+	ret = append(ret, nodes[:index]...)
+	return append(ret, nodes[index+1:]...)
+}
+
 // Step 4: Implement the Tunstall Coding using this heap.
 // Step 5: Generate a codebook from the leaves of this.
-func BuildTunstallCode(maxHeap MaxHeap, n int) map[string]string {
+/*
+func BuildTunstallCode(maxHeap *MaxHeap, n int) map[string]string {
+	// IMPLEMENT ME: Iterate from the len of the heap < 2^n
+	// Pop the highest probability node, split it into child sequences
+	// and push each sequence with the parent sequence concatenated with the child,
+	// along with the product of the parent probability and the child.
+	// Upon exiting this loop, build the tunstall code book by assigning highest occuring sequence with
+	// the lowest bit value of the n bits, i.e., if it was AAA and 2-bit code, AAA -> 00
+	parents := buildParents(maxHeap)
+	for maxHeap.Len() < (1 << n) {
+		top := heap.Pop(maxHeap).(Node)
+
+		fmt.Println(top)
+		var index int
+		var newParent Node
+		for i, v := range parents {
+			newProb := top.Probability * v.Probability
+			newPrefix := top.Sequence + v.Sequence
+
+			// make a parent node
+			if v == top {
+				index = i
+				fmt.Println(index)
+				newParent = Node{Sequence: newPrefix, Probability: newProb}
+				fmt.Println("New Parent:", newParent)
+			}
+
+			heap.Push(maxHeap, Node{Sequence: newPrefix, Probability: newProb})
+		}
+		parents = removeParent(parents, index)
+		fmt.Println(parents)
+		// Insert the parent at the index we removed. I.e., create a new slice with the elements up to parent, and append a node
+		// (the parent) at index, and append the rest of the slice following it.
+		parents = append(parents[:index], append([]Node{newParent}, parents[index:]...)...)
+		fmt.Println(parents)
+	}
 	return nil
 }
 
-// Step 6: Create an encoder, which will simply match the longest sequence, and append the corresponding
-//
-//	binary code for the matched sequence to the output.
-func TunstallEncode() string {
-	return ""
+func buildParents(maxHeap *MaxHeap) []Node {
+	var parents []Node
+	for maxHeap.Len() > 0 {
+		parents = append(parents, heap.Pop(maxHeap).(Node))
+	}
+	for _, v := range parents {
+		heap.Push(maxHeap, v)
+	}
+	return parents
+}
+
+// removeParent is a helper to remove the parent at slice index i.
+func removeParent(parent []Node, index int) []Node {
+	return append(parent[:index], parent[index+1:]...)
+}
+*/
+
+// Step 6: Create an encoder, which will simply match the longest sequence, and
+// append the corresponding binary code for the matched sequence to the output.
+func TunstallEncode(input string, codebook map[string]string) (string, error) {
+	// TODO: implement
+	encode := strings.Builder{}
+
+	return encode.String(), nil
 }
 
 // Step 7: Create a decoder, which will reverse this process.
 func TunstallDecode() string {
+	// Simply read the bits from the encoded message, and match them to a corresponding sequence
+	// in the codebook, and append that sequence to the output.
+	// TODO: Implement
 	return ""
 }
